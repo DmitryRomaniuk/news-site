9f4a5e0a0d9959c955411af2c913db5beb1e1f74
Dzmitry Ramaniuk <kanumowa@gmail.com>
2017-12-06 23:22:05 +0300

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require("webpack");
+
+const isProd = false;
+
+var path = require("path");
+module.exports = {
+  entry: {
+    app: ["./src/index.js"]
+  },
+  output: {
+    filename: "public/bundle.js",
+    path: path.resolve(__dirname, "public"),
+    publicPath: isProd ? "/public/" : `http://localhost:${7000}/public/`
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "less-loader" }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: isProd ? false : "source-map",
+  resolve: {
+    extensions: [".js"]
+  },
+  devServer: {
+    port: 7000,
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization"
+    }
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
